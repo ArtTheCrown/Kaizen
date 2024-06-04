cc7e0ba45e11f93c1129f1e29f1906e3f26e50a7
Shumail Khan <raidenhellblade@gmail.com>
2024-06-04 21:07:22 +0100

    Add project files.

---
+++ Kaizen/Models/Tasks.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Kaizen.Models
+{
+    public class Tasks
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public int Progress { get; set; }
+        public int FailedAttempts { get; set; }
+        public String Completed { get; set; }
+        public DateTime DeadLine { get; set; }
+        public List<DateTime> TimeStamps { get; set; }
+        public Configurations Configuration { get; set; }
+    }
+}
---
+++ Kaizen/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System;
+
+namespace Kaizen.Models
+{
+    public class User
+    {
+        public string Username { get; set; } = "Hyacinth";
+        public int Streak { get; set; }
+        public List<Tasks> HabitsInculcated { get; set; } = new List<Tasks>();
+        public int FailedAttempts { get; set; }
+        public List<Tasks> ActiveTasks { get; set; } = new List<Tasks>();
+
+        public void UpdateTask(int ID, string newStatus, DateTime newDeadLine)
+        {
+            var task = ActiveTasks.FirstOrDefault(t => t.ID == ID);
+            if (task != null)
+            {
+                task.Completed = newStatus;
+                task.DeadLine = newDeadLine;
+            }
+        }
+    }
+}
