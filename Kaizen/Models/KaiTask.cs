6a1cb3b1517a60ac2dd96bbe1eebd759be4a14ba
Shumail Khan <raidenhellblade@gmail.com>
2025-02-18 05:37:15 +0000

    Tasks.cs Renamed to KaiTask.cs to prevent someone overwrting issues.

---
+++ Kaizen/Models/KaiTask.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Kaizen.Models
+{
+    public class KaiTask
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public int Progress { get; set; }
+        public int FailedAttempts { get; set; }
+        public String Completed { get; set; }
+        public DateTime DeadLine { get; set; }
+        public List<DateTime> TimeStamps { get; set; }
+    }
+}
